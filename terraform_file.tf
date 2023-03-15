2c87c8fe0073bd51f89e814f749d238a56a8911a
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-15 10:32:39 +0530

    Update terraform_file.tf

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,48 +1,46 @@
-resource "aws_security_group" "example" {
-  name        = "dtcc-gpt-demo-innovation-security-group"
-  description = "Allow incoming traffic on ports 22 and 8501"
-  vpc_id      = "vpc-0a141f878afe30780"
+resource "aws_security_group" "dtcc_gpt_innovation_demo_security_group" {
+  name_prefix = "dtcc-gpt-innovation-demo-security-group"
+
+  vpc_id = "vpc-0a141f878afe30780"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8501
-    to_port     = 8501
-    protocol    = "tcp"
+    from_port = 8501
+    to_port = 8501
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0842866abbdf82e24"
+resource "aws_instance" "dtcc_gpt_innovation_demo_instance" {
+  ami = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  vpc_security_group_ids = [aws_security_group.example.id]
-  subnet_id     = "subnet-0c983406b03e8b798"
+  subnet_id = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc_gpt_innovation_demo_security_group.id]
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
-  tags = {
-    Name = "dtcc-gpt-demo-innovation"
-  }
 
   user_data = <<EOF
 #!/bin/bash
-
-# activate the pre-built pytorch environment
-source activate pytorch
-
-# install libraries "transformers" and "streamlit" via pip
-pip install transformers streamlit
+sudo apt-get update
+sudo apt-get install -y python3-pip
+pip3 install tensorflow transformers streamlit
 EOF
+
+  tags = {
+    Name = "dtcc-gpt-innovation-demo"
+  }
 }
