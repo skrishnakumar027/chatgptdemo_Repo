e5cd49338029da503875db6d2435b500faf3fb40
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-15 19:34:21 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,39 +1,39 @@
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
-  vpc_id       = "vpc-0a141f878afe30780" 
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8501
+    to_port   = 8501
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8501
-    to_port     = 8501
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id = "vpc-0a141f878afe30780"
 }
 
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami                    = "ami-0a606d8395a538502"
-  instance_type          = "g4dn.xlarge"
-  vpc_id                 = "vpc-0a141f878afe30780"
-  subnet_id              = "subnet-0c983406b03e8b798"
+  ami           = "ami-0a606d8395a538502"
+  instance_type = "g4dn.xlarge"
+  subnet_id     = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
-  key_name = "ssh-key"
-  security_group_ids     = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
-  user_data = <<-EOF
+
+  user_data = <<EOF
               #!/bin/bash
               pip install tensorflow transformers streamlit
               EOF
@@ -42,3 +42,6 @@ resource "aws_instance" "dtcc-gpt-innovation-demo" {
     Name = "dtcc-gpt-innovation-demo"
   }
 }
+``` 
+
+Note: Please make sure to input your own values for `vpc_id` and `subnet_id`. Also ensure that you have the necessary permissions and the AMI is available in your AWS account
