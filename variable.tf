6ac60f4ec1a70b076897cfbe2fd69b03c5386e71
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-15 09:09:23 +0530

    addded session token variable

--- variable.tf
+++ variable.tf
@@ -1,9 +1,12 @@
 variable "access_key" {
-  default = "AKIAXETNQAOYPLM5BJZY"
+  default = "ASIAVV73ELQ7BIC7PUVI"
   }
 variable "secret_key" {
-  default = "hj7bdgmdy95qSsyI/tkSfbl3pzQattw+rzR7G13u"
+  default = "SwGwRDrVfFkekvWHor23UCgsYnNe3G9hIs0mdbUD"
+  }
+variable "session_token" {
+  default = "IQoJb3JpZ2luX2VjECwaCXVzLWVhc3QtMiJHMEUCIQCjwKYuyL91cdL6SQwuO1VjZsQaBLQ0YzQDRDkECNNZGAIgUdgwvtWDpjVEfdzr/1bWVBH65nhwLhS1cD+7ThsA4igqigMI5f//////////ARABGgwzOTA4MzE4MjM5MzQiDPwe1O9rQ23EPfkkeCreAqD5lb2afvfzhhAZ28kUhtCj65jgch4HtkvQMZDzIOD8H9C5zThSbJGAef+zRzp0jHuwjGfqm8uIiKeFTWkPOgsGZd/qnmCiuIPR7etCUAux2d/Ni3LpbxRO/2EX/jX4fp5LxeJfeM7LlftqE/+p2waiHuNlZL/CCdJyNRSiEKUJ2LXAVnoMLXOJhht/+9Kn2JX/tCw02fhlQugqhH76M4pc542RQXYa2ppsw+Gx4+zjHtt94c/Ep7/Gkp//U5BbRKVOUp0zuTdpB7GQE9W3pagi/d/NDdRpT1/4vxjBxSE66JY3zPuKKGO63lEVASinSKyGfuULKp3OhTQLjmsMAgYr5bp6RIBH5RxoMOIXiP31H4Pl1G6IQ1QtE+4QKaXF5E1dpzMM58LJ3Nx7/U8/ZJ+6fI92akwxleULNky7hlLklRzJwoNdaZKLvMUqqqIfoYjHmih5hoTZWPYs5bFwMIn5xKAGOqYBz9mpAj1oPbqutSro7045zL58JWii1k16xOkeJdJeZIsX4i377KmHn20hjOK0Gmm2vqmdC5dy4nvLhlkeiapwbiQz/pu6x4/408pxxqiCtgAhqyp0T6eYPYESRn6Yx/uQmGidNCNzJ+bxJAhohM+98V3Qiu4GcZp3ABZd98qvZ4JPxITQWXYQTd0JbNc1iNnEVRZ0Pbd45l+tVXXi2tiz7+2hcXd/QQ=="
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-2"
   }
