5a1fb30a4f42fb0f7f24520425162e2416b512ae
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-15 19:06:40 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,46 +1,47 @@
-resource "aws_security_group" "dtcc_gpt_innovation_demo_security_group" {
+resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
-
-  vpc_id = "vpc-0a141f878afe30780"
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8501
-    to_port = 8501
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id      = "vpc-0a141f878afe30780" 
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "dtcc_gpt_innovation_demo_instance" {
-  ami = "ami-0a606d8395a538502"
-  instance_type = "g4dn.xlarge"
-  subnet_id = "subnet-0c983406b03e8b798"
-  vpc_security_group_ids = [aws_security_group.dtcc_gpt_innovation_demo_security_group.id]
+resource "aws_instance" "dtcc-gpt-innovation-demo" {
+  ami                    = "ami-0a606d8395a538502"
+  instance_type          = "g4dn.xlarge"
+  vpc_id                 = "vpc-0a141f878afe30780"
+  subnet_id              = "subnet-0c983406b03e8b798"
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
-
-  user_data = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python3-pip
-pip3 install tensorflow transformers streamlit
-EOF
+  key_name = "ssh-key"
+  security_group_ids     = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              pip install tensorflow transformers streamlit
+              EOF
 
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
 }
+``` 
+
+This should create a new security group with open ports for SSH and Streamlit, and launch a new EC2 instance of type g4dn.xlarge with libraries installed. It will also associate public IP address and use the specified VPC ID and subnet ID. Finally, it will use the `role_EC2accessViaSSM` IAM role and use user_data to install the required libraries
