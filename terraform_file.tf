3eda27cb8ac54f3a1ed1e89fd0f65e960c340b8b
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-23 18:10:18 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,47 +1,44 @@
-# Define variables
-variable "aws_region" {}
-
-# Create security group
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
-  name_prefix      = "dtcc-gpt-innovation-demo-security-group"
-  vpc_id           = "vpc-0a141f878afe30780"
+  name        = "dtcc-gpt-innovation-demo-security-group"
+  description = "Security Group for DTCC GPT Innovation Demo"
+  vpc_id      = "vpc-0a141f878afe30780"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8501
-    to_port   = 8501
-    protocol  = "tcp"
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Launch EC2 instance
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami           = "ami-0a606d8395a538502"
-  instance_type = "g4dn.xlarge"
-  subnet_id     = "subnet-0c983406b03e8b798"
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  ami                         = "ami-0a606d8395a538502"
+  instance_type               = "g4dn.xlarge"
+  subnet_id                   = "subnet-0c983406b03e8b798"
+  key_name                    = "my-ssh-key"
+  vpc_security_group_ids      = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   associate_public_ip_address = true
-  iam_instance_profile = "arn:aws:iam::${var.aws_account_id}:instance-profile/role_EC2accessViaSSM"
+  iam_instance_profile        = "role_EC2accessViaSSM"
+
   user_data = <<-EOF
               #!/bin/bash
-              pip3 install tensorflow
-              pip3 install transformers
-              pip3 install streamlit
+              pip install tensorflow transformers streamlit
               EOF
+
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
