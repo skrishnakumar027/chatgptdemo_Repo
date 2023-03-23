d38ce177a979ad7096d8a4f0cc22388bd0cb431f
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-23 17:32:06 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,48 +1,52 @@
-# Define VPC security group
-resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
+resource "aws_security_group" "demo_sg" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
 
+  vpc_id = "vpc-0a141f878afe30780"
+
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8501
-    to_port   = 8501
-    protocol  = "tcp"
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  vpc_id = "vpc-0a141f878afe30780"
 }
 
-# Launch EC2 instance
-resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami = "ami-0a606d8395a538502"
+resource "aws_instance" "demo_instance" {
+  ami           = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id = "subnet-0c983406b03e8b798"
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
-  associate_public_ip_address = true
+  subnet_id     = "subnet-0c983406b03e8b798"
+
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
   iam_instance_profile = "role_EC2accessViaSSM"
 
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
 
-  # Install libraries via user_data
-  user_data = <<-EOF
+  associate_public_ip_address = true
+
+  user_data = <<EOF
               #!/bin/bash
-              pip install tensorflow transformers streamlit
+              sudo apt update -y
+              sudo apt install python3-pip -y
+              sudo pip3 install tensorflow transformers streamlit
               EOF
 }
-``
+``` 
+
+Note: Please replace the values for the VPC ID, subnet ID, IAM role, and other resource attributes as per your requirements
