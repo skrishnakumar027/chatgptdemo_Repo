9dc9cdb1a8c6fee7ad6082b6f306554e9aa2f7ef
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-23 18:26:26 +0530

    terraform commit

---
+++ terraform_file.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
+  name_prefix = "dtcc-gpt-innovation-demo-security-group"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8501
+    to_port = 8501
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dtcc-gpt-innovation-demo" {
+  ami           = "ami-0a606d8395a538502"
+  instance_type = "g4dn.xlarge"
+  key_name      = "my-keypair"
+  subnet_id     = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  associate_public_ip_address = true
+  iam_instance_profile = "role_EC2accessViaSSM"
+ 
+  user_data = <<-EOF
+              #!/bin/bash
+              pip install tensorflow transformers streamlit
+              EOF
+}
