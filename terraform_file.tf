977bf7cd6e91107bdac50474d94ed18955a64438
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-23 15:02:00 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -2,23 +2,23 @@ resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8501
-    to_port = 8501
-    protocol = "tcp"
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -26,15 +26,22 @@ resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
 }
 
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami = "ami-0a606d8395a538502"
+  ami           = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id = "subnet-0c983406b03e8b798"
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  subnet_id     = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids      = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   associate_public_ip_address = true
-  iam_instance_profile = "role_EC2accessViaSSM"
+  iam_instance_profile        = "role_EC2accessViaSSM"
 
-  user_data = <<EOF
+  tags = {
+    Name = "dtcc-gpt-innovation-demo"
+  }
+
+  user_data = <<-EOF
               #!/bin/bash
-              pip3 install tensorflow transformers streamlit
+              pip install tensorflow transformers streamlit
               EOF
 }
+``` 
+
+Please note that the template assumes that the necessary IAM role "role_EC2accessViaSSM" is already created. Also, please make sure to provide valid VPC ID and subnet ID
