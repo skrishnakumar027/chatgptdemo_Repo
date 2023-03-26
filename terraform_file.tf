36f8535648604e616a3cd20f37934685036e001a
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 02:04:20 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,54 +1,45 @@
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
-  name        = "dtcc-gpt-innovation-demo-security-group"
-  description = "Allow inbound traffic on ports 22 and 8501 and allow all outbound traffic"
-
-  vpc_id = "vpc-0a141f878afe30780"
-
+  name_prefix = "dtcc-gpt-innovation-demo-security-group"
+  
   ingress {
-    description = "allow ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   ingress {
-    description = "allow streamlit"
-    from_port   = 8501
-    to_port     = 8501
-    protocol    = "tcp"
+    from_port = 8501
+    to_port = 8501
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  vpc_id = "vpc-0a141f878afe30780"
 }
 
-resource "aws_instance" "dtcc-gpt-innovation-demo-instance" {
-  ami           = "ami-0a606d8395a538502"
+resource "aws_instance" "dtcc-gpt-innovation-demo" {
+  ami = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id     = "subnet-0c983406b03e8b798"
-
+  subnet_id = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  associate_public_ip_address = true
+  iam_instance_profile = "role_EC2accessViaSSM"
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              pip install tensorflow transformers streamlit
+              EOF
+  
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
-
-  vpc_security_group_ids = [
-    aws_security_group.dtcc-gpt-innovation-demo-security-group.id
-  ]
-
-  iam_instance_profile = "role_EC2accessViaSSM"
-
-  user_data = <<EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install python3-pip -y
-              sudo pip3 install tensorflow transformers streamlit
-              EOF
-
-  associate_public_ip_address = true
 }
+``
