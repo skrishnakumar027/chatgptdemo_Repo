8aca638097228653eb04700d1facbc857307207b
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 02:12:18 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,38 +1,36 @@
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
-  
+
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
-    from_port = 8501
-    to_port = 8501
-    protocol = "tcp"
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   vpc_id = "vpc-0a141f878afe30780"
 }
 
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami = "ami-0a606d8395a538502"
+  ami           = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id = "subnet-0c983406b03e8b798"
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+  subnet_id     = "subnet-0c983406b03e8b798"
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
-  
   user_data = <<-EOF
               #!/bin/bash
               pip install tensorflow transformers streamlit
@@ -41,5 +39,7 @@ resource "aws_instance" "dtcc-gpt-innovation-demo" {
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
+
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
 }
 ``
