01409ad3c3aab60b7ae3f78de121c1eb5cb8c201
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 18:57:52 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -2,41 +2,35 @@ resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8501
-    to_port = 8501
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+      from_port = 8501
+      to_port = 8501
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-locals {
-  instance_name = "dtcc-gpt-innovation-demo"
-  ami_id = "ami-0a606d8395a538502"
-  instance_type = "g4dn.xlarge"
   vpc_id = "vpc-0a141f878afe30780"
-  subnet_id = "subnet-0c983406b03e8b798"
 }
 
-resource "aws_instance" "dtcc-gpt-innovation-demo-instance" {
-  ami           = local.ami_id
-  instance_type = local.instance_type
-  subnet_id     = local.subnet_id
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+resource "aws_instance" "dtcc-gpt-innovation-demo" {
+  ami           = "ami-0a606d8395a538502"
+  instance_type = "g4dn.xlarge"
   associate_public_ip_address = true
+  subnet_id = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   iam_instance_profile = "role_EC2accessViaSSM"
 
   user_data = <<-EOF
@@ -45,7 +39,9 @@ resource "aws_instance" "dtcc-gpt-innovation-demo-instance" {
               EOF
 
   tags = {
-    Name = local.instance_name
+    Name = "dtcc-gpt-innovation-demo"
   }
 }
-``
+
+```
+Note: The code assumes that the required IAM Role "role_EC2accessViaSSM" already exists in your AWS account. Also, it is highly recommended to use a provider block for better security and modulari
