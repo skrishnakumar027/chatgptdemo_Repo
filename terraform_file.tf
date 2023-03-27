b5ddc4274b9294b94b181f660172b5c92d872977
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 19:07:09 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,86 +1,45 @@
-variable "vpc_id" {
-  default = "vpc-0a141f878afe30780"
-}
-
-variable "subnet_id" {
-  default = "subnet-0c983406b03e8b798"
-}
-
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 8501
-    to_port   = 8501
-    protocol  = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  vpc_id = var.vpc_id
-}
-
-data "aws_ami" "tf-ami" {
-  most_recent = true
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["Deep Learning AMI (Amazon Linux 2) Version 45.0"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "block-device-mapping.volume-type"
-    values = ["gp2"]
-  }
+  vpc_id = "vpc-0a141f878afe30780"
 }
 
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami               = data.aws_ami.tf-ami.id
-  instance_type     = "g4dn.xlarge"
+  ami           = "ami-0a606d8395a538502"
+  instance_type = "g4dn.xlarge"
+  subnet_id     = "subnet-0c983406b03e8b798"
   associate_public_ip_address = true
-  subnet_id         = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   iam_instance_profile = "role_EC2accessViaSSM"
-  user_data         = <<-EOC
-                      #!/bin/bash
-                      pip install tensorflow transformers streamlit
-                      EOC
+
+  user_data = <<-EOF
+              #!/bin/bash
+              pip install tensorflow transformers streamlit
+              EOF
 
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
+
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
+}
