8ba8643032af3fdd79112488874823fe74da8ed2
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 18:54:48 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,43 +1,51 @@
 resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
   name_prefix = "dtcc-gpt-innovation-demo-security-group"
-  vpc_id      = "vpc-0a141f878afe30780"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8501
-    to_port     = 8501
-    protocol    = "tcp"
+    from_port = 8501
+    to_port = 8501
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami           = "ami-0a606d8395a538502"
+locals {
+  instance_name = "dtcc-gpt-innovation-demo"
+  ami_id = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id     = "subnet-0c983406b03e8b798"
+  vpc_id = "vpc-0a141f878afe30780"
+  subnet_id = "subnet-0c983406b03e8b798"
+}
+
+resource "aws_instance" "dtcc-gpt-innovation-demo-instance" {
+  ami           = local.ami_id
+  instance_type = local.instance_type
+  subnet_id     = local.subnet_id
   vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
-  user_data = <<EOF
+
+  user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install -y python3
-              sudo pip3 install tensorflow transformers streamlit
+              pip install tensorflow transformers streamlit
               EOF
+
   tags = {
-    Name = "dtcc-gpt-innovation-demo"
+    Name = local.instance_name
   }
 }
+``
