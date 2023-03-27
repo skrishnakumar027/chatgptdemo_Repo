a10726eca51d22aa9d69d2a471c73c516f9897d8
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-03-27 19:16:09 +0530

    terraform commit

--- terraform_file.tf
+++ terraform_file.tf
@@ -1,45 +1,53 @@
-resource "aws_security_group" "dtcc-gpt-innovation-demo-security-group" {
-  name_prefix = "dtcc-gpt-innovation-demo-security-group"
+resource "aws_security_group" "dtcc-gpt-innovation-demo" {
+  name        = "dtcc-gpt-innovation-demo-security-group"
+  description = "Security group for DTCC GPT Innovation demo"
+
+  vpc_id      = "vpc-0a141f878afe30780"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8501
-    to_port     = 8501
-    protocol    = "tcp"
+    from_port = 8501
+    to_port   = 8501
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  vpc_id = "vpc-0a141f878afe30780"
 }
 
 resource "aws_instance" "dtcc-gpt-innovation-demo" {
-  ami           = "ami-0a606d8395a538502"
+  ami = "ami-0a606d8395a538502"
   instance_type = "g4dn.xlarge"
-  subnet_id     = "subnet-0c983406b03e8b798"
+  key_name = "mykeypair"
+
+  subnet_id = "subnet-0c983406b03e8b798"
+  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo.id]
+
   associate_public_ip_address = true
   iam_instance_profile = "role_EC2accessViaSSM"
 
   user_data = <<-EOF
               #!/bin/bash
-              pip install tensorflow transformers streamlit
+              sudo apt-get update
+              sudo apt-get install -y python3-pip
+              pip3 install tensorflow transformers streamlit
               EOF
 
   tags = {
     Name = "dtcc-gpt-innovation-demo"
   }
-
-  vpc_security_group_ids = [aws_security_group.dtcc-gpt-innovation-demo-security-group.id]
 }
+``` 
+
+Please note that you will need to have proper authentication set up to use AWS provider in Terraform. Also, make sure to replace the values of `vpc_id`, `subnet_id`, and `iam_instance_profile` with the actual values you have in your AWS account
